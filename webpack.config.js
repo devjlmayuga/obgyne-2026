0306c2f96fe109837b40ffd62266cf88bd0efd6f
johnmayuga <john.mayuga@tooltwist.com>
2026-06-01 20:32:06 +0800

    chore: upgrade tooling for Node 24

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const outputDirectory = 'dist';
 
 module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'],
+  entry: ['core-js/stable', 'regenerator-runtime/runtime', './src/client/index.js'],
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js'
@@ -28,25 +28,36 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
-        loader: 'url-loader?limit=100000'
+        test: /\.(png|woff2?|eot|ttf|svg|jpg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100000
+          }
+        }
       }
     ]
   },
   devServer: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': 'http://ec2-13-229-215-142.ap-southeast-1.compute.amazonaws.com'
-    },
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://ec2-13-229-215-142.ap-southeast-1.compute.amazonaws.com',
+        changeOrigin: true
+      }
+    ],
     hot: true,
-    inline: true,
-    watchOptions: {
-      poll: true
+    watchFiles: {
+      paths: ['src/**/*', 'public/**/*'],
+      options: {
+        poll: true
+      }
     }
   },
   plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
