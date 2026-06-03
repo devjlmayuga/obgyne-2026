ff9e147357a35b66b2fec52fd036c72888b11a72
johnmayuga <john.mayuga@tooltwist.com>
2026-06-03 16:48:41 +0800

    feat: update API_URL to point to the production server

--- src/client/configuration/client_config.js
+++ src/client/configuration/client_config.js
@@ -1,4 +1,4 @@
-export const API_URL = '';
+export const API_URL = 'https://lcontreras-obgyne.asia';
 export const API_URL_LOCAL = 'http://localhost:8080';
 // DEV
 // export const API_URL = 'http://ec2-13-229-215-142.ap-southeast-1.compute.amazonaws.com';
